a04f515a3a9abd7bf0866f5879d09f204c08fcfe
Reuben Settergren <ruberad@gmail.com>
2017-08-13 16:25:03 -0700

    Add single quotes around my name

--- MyName.py
+++ MyName.py
@@ -5,3 +5,23 @@
 
 print("My name is 'RubeRad'")
 
+
+# Create a new (local) branch
+# Optionally VCS/Git/Push to test you can cause your local branch
+#     to be created also on the upstream repository at github
+# Add a print statement to this file below, save, test
+# VCS/GIT/Commit... (to your local repository)
+#   Enter a meaningful commit message!!!!
+# VCS/GIT/Push...   (your changes to the upstream repo)
+#   Note, "Commit & Push" is an option on Commit...
+# VCS/GIT/Create Pull Request...
+
+# wait for a github repo administrator to merge your branch
+#    to the upstream repo's master branch
+
+# Change PyCharm to the master branch
+# VCS/GIT/Pull...
+# Your changes should be available (and everybody else's!)
+
+
+# what is your name?
